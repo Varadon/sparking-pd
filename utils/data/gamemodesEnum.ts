91dbe82e9ec9d1d9653b201ad1e90740fc23643f
Varadon <frankshizuo07@gmail.com>
2024-10-15 18:41:51 +0200

    feat: get single, costless and team characters with selectable members number

---
+++ utils/data/gamemodesEnum.ts
@@ -0,0 +1,5 @@
+export enum gameModes {
+  Single = <number>0,
+  Team = <number>1,
+  CostlessTeam = <number>2,
+}
--- utils/getDataMethods.ts
+++ utils/getDataMethods.ts
@@ -1,5 +1,35 @@
-export const getRandomCharacter = () => {};
+import { sparkingData } from "./data/sparkingData";
+import { SparkingDataElement } from "./data/types";
 
-export const getRandomTeam = (pd: number) => {};
+export const getRandomCharacter = () => {
+  const randomIndex = Math.floor(Math.random() * sparkingData.length);
+  return [sparkingData[randomIndex]];
+};
 
-export const getCostlessRandomTeam = () => {};
+export const getRandomTeam = (pd: number) => {
+  const team: SparkingDataElement[] = [];
+  let characters = [...sparkingData];
+  let remainingPd = pd;
+
+  while (remainingPd > 0 && characters.length > 0) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    team.push(characters[randomIndex]);
+    remainingPd -= characters[randomIndex].cost;
+    characters.splice(randomIndex, 1);
+    characters = characters.filter((el) => el.cost <= remainingPd);
+  }
+
+  return team;
+};
+
+export const getCostlessRandomTeam = (members: number) => {
+  const team: SparkingDataElement[] = [];
+  const characters = [...sparkingData];
+
+  for (let i = 0; i < members; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    team.push(characters[randomIndex]);
+    characters.splice(randomIndex, 1);
+  }
+  return team;
+};
