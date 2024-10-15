3043a3a9794e150fec24d81cc55541fdf298ca68
Varadon <frankshizuo07@gmail.com>
2024-10-16 00:56:46 +0200

    style: initial page and fixed image size

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       case 2:
         setCharacters(
           getCostlessRandomTeam(Number(membersRef.current?.value) || 5)
-        ); //members number
+        );
         break;
       default:
         break;
@@ -38,48 +38,49 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button onClick={getCharacter} className="outline bg-white text-black">
-        Roll Characters
-      </button>
-      <select
-        name="gamemode"
-        className="text-black"
-        defaultValue={gameModes.Single}
-        onChange={changeGamemode}
-      >
-        <option value={gameModes.Single}> Singolo </option>
-        <option value={gameModes.Team}>Team PD</option>
-        <option value={gameModes.CostlessTeam}>Team senza PD</option>
-      </select>
-      {gameMode === gameModes.CostlessTeam && (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex justify-center items-center gap-4">
+        <button onClick={getCharacter} className="outline bg-white text-black">
+          Roll Characters
+        </button>
         <select
-          name="members"
+          name="gamemode"
           className="text-black"
-          defaultValue={5}
-          ref={membersRef}
+          defaultValue={gameModes.Single}
+          onChange={changeGamemode}
         >
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
+          <option value={gameModes.Single}> Singolo </option>
+          <option value={gameModes.Team}>Team PD</option>
+          <option value={gameModes.CostlessTeam}>Team senza PD</option>
         </select>
-      )}
+        {gameMode === gameModes.CostlessTeam && (
+          <select
+            name="members"
+            className="text-black"
+            defaultValue={5}
+            ref={membersRef}
+          >
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+        )}
+      </div>
+
       <div className="flex justify-items-center items-center">
         {characters.map((character) => (
           <div key={character.id} className="m-4">
-            <div>id : {character.id}</div>
-            <div>name: {character.name}</div>
-            <div>cost: {character.cost}</div>
             <Image
-              className="w-auto h-auto"
+              className="w-48 h-48 object-cover"
               src={`/${character.img}.png`}
               width={300}
               height={300}
               alt="img"
               priority
             />
+            <div className="text-center">{character.name}</div>
           </div>
         ))}
       </div>
